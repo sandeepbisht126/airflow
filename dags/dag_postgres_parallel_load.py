3ed73bdcb3b5305017130a91861e7a427cc0550a
sandeepbisht126 <sandeep.bisht126@gmail.com>
2025-04-18 23:37:30 +0530

    set state of tasks in dag

--- dags/dag_postgres_parallel_load.py
+++ dags/dag_postgres_parallel_load.py
@@ -60,7 +60,7 @@ with DAG(
     )
 
     all_tasks = []
-    for cc in ['IN', 'ID', 'HK']:
+    for cc in ['IN', 'ID', 'HK', 'PK', 'PH']:
         if cc == 'IN':
             parallel_load(cc)
         else:
